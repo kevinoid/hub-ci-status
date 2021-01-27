de3a18673c5b82668a85996c45a6be250beede45
Kevin Locke <kevin@kevinlocke.name>
2021-01-26 19:17:20 -0700

    refactor: async-ify git-init
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test-lib/git-init.js
+++ test-lib/git-init.js
@@ -13,37 +13,38 @@ const execFileOut = require('../lib/exec-file-out.js');
 
 const rimrafP = promisify(rimraf);
 
-module.exports = function gitInit(repoPath, defaultBranch) {
-  return rimrafP(repoPath)
-    .then(async () => {
-      try {
-        await execFileOut(
-          'git',
-          // git-init(1) in 2.30.0 warns that default branch subject to change.
-          // It may also have non-default global- or user-configuration.
-          // Specify --initial-branch to avoid depending on default
-          ['init', '-q', `--initial-branch=${defaultBranch}`, repoPath],
-        );
-      } catch {
-        // git < 2.28.0 doesn't understand --initial-branch, default is master
-        await execFileOut('git', ['init', '-q', repoPath]);
-        if (defaultBranch !== 'master') {
-          await execFileOut(
-            'git',
-            ['symbolic-ref', 'HEAD', `refs/heads/${defaultBranch}`],
-            { cwd: repoPath },
-          );
-        }
-      }
-    })
-    // The user name and email must be configured for the later git commands
-    // to work.  On Travis CI (and probably others) there is no global config
-    .then(() => execFileOut(
+module.exports = async function gitInit(repoPath, defaultBranch) {
+  await rimrafP(repoPath);
+
+  try {
+    await execFileOut(
       'git',
-      ['-C', repoPath, 'config', 'user.name', 'Test User'],
-    ))
-    .then(() => execFileOut(
-      'git',
-      ['-C', repoPath, 'config', 'user.email', 'test@example.com'],
-    ));
+      // git-init(1) in 2.30.0 warns that default branch subject to change.
+      // It may also have non-default global- or user-configuration.
+      // Specify --initial-branch to avoid depending on default
+      ['init', '-q', `--initial-branch=${defaultBranch}`, repoPath],
+    );
+  } catch {
+    // git < 2.28.0 doesn't understand --initial-branch, default is master
+    await execFileOut('git', ['init', '-q', repoPath]);
+    if (defaultBranch !== 'master') {
+      await execFileOut(
+        'git',
+        ['symbolic-ref', 'HEAD', `refs/heads/${defaultBranch}`],
+        { cwd: repoPath },
+      );
+    }
+  }
+
+  // The user name and email must be configured for the later git commands
+  // to work.  On Travis CI (and probably others) there is no global config
+  await execFileOut(
+    'git',
+    ['-C', repoPath, 'config', 'user.name', 'Test User'],
+  );
+
+  await execFileOut(
+    'git',
+    ['-C', repoPath, 'config', 'user.email', 'test@example.com'],
+  );
 };

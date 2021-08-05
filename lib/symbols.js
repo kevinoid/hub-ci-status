kevinoid/node-project-template

Convert to `type: module`
Convert to native ECMAScript Modules, supported since Node.js 12.17.
This gains the benefits of native `import`/`export` for faster
asynchronous loading, cycle detection, and tree shaking in bundlers.
Unfortunately, it will create some pain for dependencies, since ESM can
not be imported using `require()` and must be imported using `import()`,
which operates asynchronously and returns a `Promise`.

See discussion in https://github.com/sindresorhus/meta/discussions/15

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

lib/symbols.js
@@ -6,10 +6,9 @@
  * This file is intentionally excluded from package.json#exports.
  */
 
-'use strict';
-
 /** Symbol of mock function used in place of modulename for testing.
  *
  * @private
  */
-exports.modulenameMockSymbol = Symbol('modulenameMock');
+// eslint-disable-next-line import/prefer-default-export
+export const modulenameMockSymbol = Symbol('modulename');

kevinoid/hub-ci-status

lib/symbols.js
@@ -10,34 +10,34 @@
  *
  * @private
  */
-exports.HttpAgentMockSymbol = Symbol('HttpAgent');
+export const HttpAgentMockSymbol = Symbol('HttpAgent');
 
 /** Symbol of mock function used in place of https.Agent for testing.
  *
  * @private
  */
-exports.HttpsAgentMockSymbol = Symbol('HttpsAgent');
+export const HttpsAgentMockSymbol = Symbol('HttpsAgent');
 
 /** Symbol of mock function used in place of Octokit for testing.
  *
  * @private
  */
-exports.OctokitMockSymbol = Symbol('Octokit');
+export const OctokitMockSymbol = Symbol('Octokit');
 
 /** Symbol of mock function used in place of fetchCiStatus for testing.
  *
  * @private
  */
-exports.fetchCiStatusMockSymbol = Symbol('fetchCiStatus');
+export const fetchCiStatusMockSymbol = Symbol('fetchCiStatus');
 
 /** Symbol of mock function used in place of getProjectName for testing.
  *
  * @private
  */
-exports.getProjectNameMockSymbol = Symbol('getProjectName');
+export const getProjectNameMockSymbol = Symbol('getProjectName');
 
 /** Symbol of mock function used in place of resolveCommit for testing.
  *
  * @private
  */
-exports.resolveCommitMockSymbol = Symbol('resolveCommit');
+export const resolveCommitMockSymbol = Symbol('resolveCommit');
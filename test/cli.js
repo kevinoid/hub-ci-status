kevinoid/node-project-template

Improve assertion error in test/cli.js
Since only the first assertion failure is reported, check assertions in
the order that is most useful for debugging:  Check stderr messages,
then stdout content, then exit code.  It's much nicer to see the stderr
messages than `AssertionError: 0 !== 1` when an unexpected error occurs.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

test/cli.js
@@ -100,12 +100,12 @@ describe('modulename/cli.js', () => {
   it('writes error and exit 1 for unexpected option', async () => {
     const options = getTestOptions();
     const code = await main([...sharedArgs, '--unexpected'], options);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(
       options.stderr.read(),
       "error: unknown option '--unexpected'\n",
     );
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 1);
   });
 
   for (const helpOption of ['-h', '--help']) {
@@ -135,12 +135,12 @@ Options:
       const packageJson = await packageJsonPromise;
       const options = getTestOptions();
       const code = await main([...sharedArgs, verOption], options);
-      assert.strictEqual(code, 0);
+      assert.strictEqual(options.stderr.read(), null);
       assert.strictEqual(
         options.stdout.read(),
         `${packageJson.version}\n`,
       );
-      assert.strictEqual(options.stderr.read(), null);
+      assert.strictEqual(code, 0);
     });
   }
 
@@ -149,9 +149,9 @@ Options:
     const err = new Error('test');
     options[modulenameMockSymbol] = () => Promise.reject(err);
     const code = await main(sharedArgs, options);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), `${err}\n`);
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 1);
   });
 
   it('writes error to stderr and exit 1 on throw', async () => {
@@ -159,9 +159,9 @@ Options:
     const err = new Error('test');
     options[modulenameMockSymbol] = () => { throw err; };
     const code = await main(sharedArgs, options);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), `${err}\n`);
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 1);
   });
 
   it('passes non-option arguments as options.files', async () => {
@@ -171,9 +171,9 @@ Options:
       assert.deepStrictEqual(opts.files, args);
     };
     const code = await main([...sharedArgs, ...args], options);
-    assert.strictEqual(code, 0);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), null);
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 0);
   });
 
   it('verbosity: -1 for --quiet', async () => {
@@ -182,9 +182,9 @@ Options:
       assert.strictEqual(opts.verbosity, -1);
     };
     const code = await main([...sharedArgs, '--quiet'], options);
-    assert.strictEqual(code, 0);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), null);
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 0);
   });
 
   it('verbosity: 1 for --verbose', async () => {
@@ -193,8 +193,8 @@ Options:
       assert.strictEqual(opts.verbosity, 1);
     };
     const code = await main([...sharedArgs, '--verbose'], options);
-    assert.strictEqual(code, 0);
-    assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), null);
+    assert.strictEqual(options.stdout.read(), null);
+    assert.strictEqual(code, 0);
   });
 });

kevinoid/hub-ci-status

test/cli.js
@@ -89,11 +89,11 @@ describe('hub-ci-status command', () => {
       const args = [...RUNTIME_ARGS, helpOpt];
       const options = getTestOptions();
       const exitCode = await hubCiStatusCmd(args, options);
-      assert.strictEqual(exitCode, 0);
       assert.strictEqual(options.stderr.read(), null);
       const output = options.stdout.read();
       assert(output, 'produces help output');
       assert.match(output, /--wait\b/);
+      assert.strictEqual(exitCode, 0);
     });
   }
 
@@ -103,10 +103,10 @@ describe('hub-ci-status command', () => {
       const args = [...RUNTIME_ARGS, versionOpt];
       const options = getTestOptions();
       const exitCode = await hubCiStatusCmd(args, options);
-      assert.strictEqual(exitCode, 0);
       assert.strictEqual(options.stderr.read(), null);
       const output = options.stdout.read();
       assert.strictEqual(output, `${packageJson.version}\n`);
+      assert.strictEqual(exitCode, 0);
     });
   }
 
@@ -262,9 +262,9 @@ describe('hub-ci-status command', () => {
       const allArgs = [...RUNTIME_ARGS, ...args];
       const options = getTestOptions();
       const exitCode = await hubCiStatusCmd(allArgs, options);
-      assert.strictEqual(exitCode, 1);
       assert.strictEqual(options.stdout.read(), null);
       assert.match(options.stderr.read(), expectErrMsg);
+      assert.strictEqual(exitCode, 1);
     });
   }
 
@@ -293,9 +293,9 @@ describe('hub-ci-status command', () => {
       hubCiStatus,
     };
     const exitCode = await hubCiStatusCmd(RUNTIME_ARGS, options);
-    assert.strictEqual(exitCode, 1);
     assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), `${errTest}\n`);
+    assert.strictEqual(exitCode, 1);
   });
 
   it('prints hubCiStatus rejection stack if very verbose', async () => {
@@ -307,8 +307,8 @@ describe('hub-ci-status command', () => {
       hubCiStatus,
     };
     const exitCode = await hubCiStatusCmd(args, options);
-    assert.strictEqual(exitCode, 1);
     assert.strictEqual(options.stdout.read(), null);
     assert.strictEqual(options.stderr.read(), `${errTest.stack}\n`);
+    assert.strictEqual(exitCode, 1);
   });
 });